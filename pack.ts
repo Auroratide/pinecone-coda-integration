67799af0668a0f8fd1e3631aaa9ca6c7d3030fc3
Moiri Gamboni <moiri.gamboni@proton.me>
2025-09-06 20:04:10 +0100

    CI: set pack version in separate file

--- pack.ts
+++ pack.ts
@@ -1,9 +1,16 @@
 import * as coda from "@codahq/packs-sdk"
 import { EmbedVector } from "./pack/EmbedVector"
 import { QuerySimilar } from "./pack/QuerySimilar"
+import { readFileSync } from "fs"
 
 export const pack = coda.newPack()
 
+try {
+  const version = readFileSync("VERSION", "utf8").trim()
+  if (version) {
+    pack.setVersion(version)
+  }
+} catch {}
 pack.addNetworkDomain("pinecone.io")
 
 pack.setUserAuthentication({
